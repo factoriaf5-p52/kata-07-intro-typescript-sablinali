f08d834adb9a1dc721cfe9cfc7a7951fdbf833e0
Sablinita <sablinahare@gmail.com>
2022-12-05 01:01:08 +0100

    done9.1

---
+++ assignment9.1-9.3/bmiCalculator.ts
@@ -0,0 +1,10 @@
+let a = 180;
+let b = 74;
+
+
+const calculateBmi = (height, weight) => {
+
+    return "Normal (healthy weight)";
+}
+
+console.log(calculateBmi(a, b));
--- multiplier.ts
+++ multiplier.ts
@@ -26,28 +26,70 @@
 
 // console.log(calculator (100, 10, 'module'));
 
-type Operation = 'multiply' | 'add' | 'divide';
+// type Operation = 'multiply' | 'add' | 'divide';
 
-type Result = number;
+// type Result = number;
 
-const calculator = (a: number, b: number, op: Operation) : Result => {
-  switch(op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
+// const calculator = (a: number, b: number, op: Operation) : Result => {
+//   switch(op) {
+//     case 'multiply':
+//       return a * b;
+//     case 'divide':
+//       if (b === 0) throw new Error('Can\'t divide by 0!');
+//       return a / b;
+//     case 'add':
+//       return a + b;
+//     default:
+//       throw new Error('Operation is not multiply, add or divide!');
+//   }
+// }
+
+// try {
+//   console.log(calculator(1, 5 , 'divide'));
+// } catch (error: unknown) {
+//   let errorMessage = 'Something went wrong.'
+//   if (error instanceof Error) {
+//     errorMessage += ' Error: ' + error.message;
+//   }
+//   console.log(errorMessage);
+// }
+
+// const multiplicator = (a: number, b: number, printText: string) => {
+//     console.log(printText,  a * b);
+//   }
+  
+//   const a: number = Number(process.argv[2])
+//   const b: number = Number(process.argv[3])
+//   multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
+
+interface MultiplyValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
 }
 
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText,  a * b);
+}
+
 try {
-  console.log(calculator(1, 5 , 'divide'));
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
 } catch (error: unknown) {
-  let errorMessage = 'Something went wrong.'
+  let errorMessage = 'Something bad happened.'
   if (error instanceof Error) {
     errorMessage += ' Error: ' + error.message;
   }

d93926c95667161fbd83301b87d7d00eaad392be
Sablinita <sablinahare@gmail.com>
2022-12-26 21:21:01 +0100

    finished my kata 7

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -10,23 +10,26 @@
 // console.log(calculateBmi(a, b));
 
 
-export function bmiCalculator(){}
+export function bmiCalculator(height: number, weight: number) {
 
-let calculateBmi = (height: number, weight: number): string => {
     let bmi = weight / ((height / 100)** 2);
     //let bmi = weight / (height * height);
     if (bmi < 18.4){
-        return "underweight"
+        return "Underweight";
     }
     if (bmi >= 18.4 && bmi <= 24.9){
-        return "normal"
+        return "Normal";
     }
     if (bmi >= 25 && bmi <= 29.9){
-        return "overweight"
+        return "Overweight";
     }
     if (bmi >= 30){
-        return "obese"
+        return "Obese";
     }
+    return ""
 }
-console.log(calculateBmi(156, 50))
+
+ console.log(bmiCalculator(171, 68));
+
+
 
